1e801346b575e3ed36aaf4dc47cc08132a38fa2c
kenneth <ken.jav550@gmail.com>
2025-07-24 02:17:06 -0500

    feat: set dataGen

---
+++ ItemsMod-Forge/src/main/java/com/kenikydev/kenikyitems/datgen/DtaGenerators.java
@@ -0,0 +1,31 @@
+package com.kenikydev.kenikyitems.datgen;
+
+import com.kenikydev.kenikyitems.KenikyItems;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@Mod.EventBusSubscriber(modid = KenikyItems.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DtaGenerators {
+
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator gen = event.getGenerator();
+        PackOutput packOutput = gen.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        gen.addProvider(event.includeServer(), new LootTableProvider(packOutput, Collections.emptySet(), List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
+    }
+
+}
---
+++ ItemsMod-Forge/src/main/java/com/kenikydev/kenikyitems/datgen/ModItemModelProvider.java
@@ -0,0 +1,4 @@
+package com.kenikydev.kenikyitems.datgen;
+
+public class ModItemModelProvider {
+}
